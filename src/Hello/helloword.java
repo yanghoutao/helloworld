4470b635e764e811e04b02b8981f7e8f41e428a3
yht <789@itcast>
2019-11-28 22:07:26 +0800

    第二次提交

---
+++ src/Hello/helloword.java
@@ -0,0 +1,16 @@
+package Hello;
+
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+    }
+}
